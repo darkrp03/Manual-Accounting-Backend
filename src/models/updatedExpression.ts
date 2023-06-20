5028e2396471543b51c4e1a56fe99eda880a8d54
Andrew <andrejveselyj90@gmail.com>
2023-06-20 14:53:12 +0300

    deleted files

--- src/models/updatedExpression.ts
+++
@@ -1,9 +0,0 @@
-export class DynamoDBUpdateArgs {
-    updateExpression?: string;
-    expressionAttributeValues?: {[key: string]: any};
-
-    constructor(updateExpression: string, expressionAttributeValues: {[key: string]: any}) {
-        this.updateExpression = updateExpression;
-        this.expressionAttributeValues = expressionAttributeValues;
-    }
-}
