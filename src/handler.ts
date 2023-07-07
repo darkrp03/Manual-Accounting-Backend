45d889a62294d526535cb9097ee9dce2e9bbe65a
Andrii <andrejveselyj90@gmail.com>
2023-07-07 14:59:11 +0300

    added file to describe serverless functions

---
+++ src/handler.ts
@@ -0,0 +1,18 @@
+import serverless from 'serverless-http';
+import { app } from './app';
+
+const handler = serverless(app);
+const serverlessCallback = async (context: any, req: any) => {
+    context.res = await handler(context, req);
+
+    return context.res;
+};
+
+export const getVisitors = serverlessCallback;
+export const getVisitor = serverlessCallback;
+export const addVisitor = serverlessCallback;
+export const deleteVisitor = serverlessCallback;
+export const updateVisitor = serverlessCallback;
+export const login = serverlessCallback;
+export const user = serverlessCallback;
+export const logout = serverlessCallback;
